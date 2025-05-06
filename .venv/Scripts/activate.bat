8ce8a10c912dd617f5bd8088cd6ca2fef504dbc5
Avichal-nath <avichalnath314@gmail.com>
2025-05-06 18:07:47 +1200

    Set up personal virtual environment: removed existing .venv, created new .venv, and installed dependencies from profile_service/requirements.txt

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,15 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jayshil.singh\Desktop\CS415_A3\.venv
+set VIRTUAL_ENV=C:\Users\avichal avneel nath\Documents\Semester 7\CS415\A3\CS415_A3\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(.venv) %PROMPT%
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(.venv) %PROMPT%"
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(.venv) 
+set VIRTUAL_ENV_PROMPT=.venv
 
 :END
 if defined _OLD_CODEPAGE (
